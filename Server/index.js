3ce3faeab67a1eb8dfb440a6326095a4f7fd2fee
Marois2000 <tm1207@plymouth.edu>
2023-10-26 22:18:00 -0400

    started working on routes

--- Server/index.js
+++ Server/index.js
@@ -2,13 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const pool = require("./db");
+const path = "localhost/";
 
 //MIDDLEWARE
 app.use(cors());
 app.use(express.json());
 
 
+// Get a User
+app.get(path+'trendsync/login:5000', (req, res) => {
+    const {email, password} = req;
+    const query = pool.query("SELECT * FROM users WHERE email=$1 AND password=$2;", [email, password]);
 
+    res = query.rows;
+});
 
 
 
